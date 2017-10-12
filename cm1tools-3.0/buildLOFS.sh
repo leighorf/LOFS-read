a2e120e4f69b3a3d8e4b641a7255bf4bb4101688
Leigh Orf <leigh.orf@ssec.wisc.edu>
2017-10-12 14:13:40 -0500

    Added build script in cm1tools-3.0 directory so that the tools are built and installed. Tested with new Makefile.zfp in cm1tools-3.0 directory on Blue Waters.
    	deleted:    cm1tools-3.0/Makefile
    	new file:   cm1tools-3.0/Makefile.zfp
    	new file:   cm1tools-3.0/buildLOFS.sh

---
+++ cm1tools-3.0/buildLOFS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+module unload darshan
+
+module use /sw/bw/thg/modulefiles
+module load phdf5
+module load zfp
+module load h5zzfp
+module load szip
+module load cray-netcdf
+
+make -f Makefile.zfp install
+
