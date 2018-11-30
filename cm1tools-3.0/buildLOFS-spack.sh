d6124c51651de45d5ee2867e83e4b400550e2ff2
keltonhalbert <keltonhalbert@ou.edu>
2018-11-30 21:24:36 +0000

    added makefile and build script to use Spack

---
+++ cm1tools-3.0/buildLOFS-spack.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+## KELTON NOTE:
+## This is an install script for 
+## LOFS using Spack to handle the 
+## package management. I can't
+## seem to figure out how to do this
+## natively in Spack just yet but this
+## will do the trick for now. I think.
+
+## This assumes the spack  binary is 
+## already in your user path. If it isn't,
+## add '. /path/to/spack/share/setup-env.sh' 
+## to your ~/.bash_profile or ~/.bashrc
+
+
+
+## Install HDF5 first
+echo "Installing LOFS Dependencies"
+echo "Installing OpenMPI"
+spack install openmpi
+echo "Installing HDF5"
+spack install hdf5@1.10.3+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe 
+echo "Installing NetCDF"
+spack install netcdf@4.6.1~dap~hdf4+mpi~parallel-netcdf+shared ^hdf5@1.10.3+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
+echo "Installing NetCDF C++ and Fortran APIs"
+spack install netcdf-cxx4@4.3.0 ^hdf5@1.10.3+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
+spack install netcdf-fortran@4.4.4 ^hdf5@1.10.3+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
+echo "Installing ZFP Plugin for HDF5"
+spack install h5z-zfp@0.8.0+fortran ^hdf5@1.10.3+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
+
+
+echo "Loading module files"
+spack load hdf5
+spack load netcdf
+spack load netcdf-fortran
+spack load netcdf-cxx4
+spack load h5z-zfp
+spack load zfp
+spack load openmpi
+
+echo "Building LOFS"
+make -f Makefile.spack install
