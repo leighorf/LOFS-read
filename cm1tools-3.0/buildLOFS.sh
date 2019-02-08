cf1e21de49e160c4453ebf5d3479dc6bf9b0b051
keltonhalbert <kthalbert@wisc.edu>
2019-02-08 15:28:44 -0600

    fixed build script bug

--- cm1tools-3.0/buildLOFS.sh
+++ cm1tools-3.0/buildLOFS.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 module unload darshan
 
-if ["$1" == "--local"]; then
+if [ "$1" == "--local" ]; then
     echo "Building LOFS from local libraries..."
     module load cray-hdf5-parallel
     module load cray-netcdf-hdf5parallel
-    make -f Makefile.bw.local
+    make -f Makefile.bw.local all
+    make -f Makefile.bw.local install
 else
     echo "Building LOFS from system libraries..."
     module use /sw/bw/thg/modulefiles
