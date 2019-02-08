dd79a8151bd6a43dc567fb6c86d496ea435296a4
keltonhalbert <kthalbert@wisc.edu>
2019-02-08 14:49:19 -0600

    Added option --local to build LOFS using local libraries

--- cm1tools-3.0/buildLOFS.sh
+++ cm1tools-3.0/buildLOFS.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 module unload darshan
 
-module use /sw/bw/thg/modulefiles
-module load phdf5
-module load zfp
-module load h5zzfp
-module load szip
-module load cray-netcdf
-
-make -f Makefile.bw install
-
+if ["$1" == "--local"]; then
+    echo "Building LOFS from local libraries..."
+    module load cray-hdf5-parallel
+    module load cray-netcdf-hdf5parallel
+    make -f Makefile.bw.local
+else
+    echo "Building LOFS from system libraries..."
+    module use /sw/bw/thg/modulefiles
+    module load phdf5/1.8.18
+    module load zfp
+    module load h5zzfp/1.8.18
+    module load szip
+    #module load cray-netcdf
+    module load cray-netcdf-hdf5parallel
+    make -f Makefile.bw install
+fi
