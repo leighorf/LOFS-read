2a57468eab5fed9784d3c8a16729c274d4c2bfd0
keltonhalbert <kthalbert@wisc.edu>
2020-04-09 18:08:17 -0500

    Trying to get our compilation steps more streamlined. On frontera we use Intel, but on our local systems we use gcc. When using Spack, using the intel compiler required to additional dynamic linking. This is probably something that could and should be handled in a single Makefile, but I'm not very knowledgable here. I discovered that 'spack find' can be asked to return the compiler used, and can use grep to determine if the compiler shows up in the string as a means of checking which Makefile to use. It's elegant enough, I think. So, the install script checks what ZFP was compiled with and uses the appropriate Makefile. This shouldn't break anything. Famous last words...

--- buildLOFS-spack.sh
+++ buildLOFS-spack.sh
@@ -4,6 +4,18 @@ echo "Loading module files"
 spack load netcdf-c
 spack load h5z-zfp
 cd src
-PREFIX=$PREFIX make -f Makefile.spack clean 
-PREFIX=$PREFIX make -f Makefile.spack all
-PREFIX=$PREFIX make -f Makefile.spack install
+
+
+if spack find --format "{compiler}" zfp | grep -q intel; then
+    echo "It appears ZFP was compiled with Intel. Using Intel Makefile..."
+    PREFIX=$PREFIX make -f Makefile.spack.intel clean 
+    PREFIX=$PREFIX make -f Makefile.spack.intel all
+    PREFIX=$PREFIX make -f Makefile.spack.intel install
+elif spack find --format "{compiler}" zfp | grep -q intel; then
+    PREFIX=$PREFIX make -f Makefile.spack.gcc clean 
+    PREFIX=$PREFIX make -f Makefile.spack.gcc all
+    PREFIX=$PREFIX make -f Makefile.spack.gcc install
+else
+    echo "ZFP wasn't compiled by either gcc or intel, which are currently the only supported configurations. You may have to manually modify the Makefile to work."
+fi
+
