6f3c0a22aa446a33044f4580ff8b20a47037642c
Leigh Orf <leigh.orf@ssec.wisc.edu>
2020-04-21 14:02:18 -0500

    Ran into a backward compaitibility issue with on orf.ssec because I am using a git repo on the devel branch for zfp on Frontera. Thankfully spack has an option (develop) that mimics this exact behavior, building the latest git version. So we'll be using the latest ZFP code by default.

--- buildLOFS-dependencies.sh
+++ buildLOFS-dependencies.sh
@@ -9,6 +9,6 @@ echo "Installing NetCDF C++ and Fortran APIs"
 spack install netcdf-cxx4@4.3.1 ^hdf5@1.10.6+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
 spack install netcdf-fortran@4.5.2 ^hdf5@1.10.6+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
 echo "Installing ZFP Plugin for HDF5"
-spack install h5z-zfp@0.8.0+fortran ^hdf5@1.10.6+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
+spack install h5z-zfp@develop+fortran ^hdf5@1.10.6+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
 echo "Installing ncview"
 spack install ncview ^netcdf-c@4.7.3~dap~hdf4+mpi~parallel-netcdf+shared ^hdf5@1.10.6+cxx~debug+fortran+hl+mpi+pic+shared+szip~threadsafe
